cc630cc4d425b497b6229607d2feec124f7a355b
nickhans <nickhanselman@icloud.com>
2018-11-23 01:52:44 -0600

    removed and cleaned print statements used for testing

--- Generator.cpp
+++ Generator.cpp
@@ -8,12 +8,11 @@ void Generator::addEdge(std::string w1, std::string w2) {
       this->addVertex(w2);
     vertex* temp1 = this->findVertex(w1);
     vertex* temp2 = this->findVertex(w2);
-    std::cout << temp1->word << " " << temp2->word << std::endl;
-    std::cout << "Edge - " << w1 << " --> " << w2 << " created." << std::endl;
+    std::cout << "Edge - " << w1 << " --> " << w2 << " - created." << std::endl;
   } else {
     edge* temp = this->findEdge(w1, w2);
     temp->count++;
-    std::cout << "Edge - " << w1 << " --> " << w2 << " incremented."
+    std::cout << "Edge - " << w1 << " --> " << w2 << " - incremented."
               << std::endl;
   }
 }
@@ -47,10 +46,8 @@ edge* Generator::findEdge(std::string w1, std::string w2) {
   if (this->isVertex(w1) && this->isVertex(w2)) {
     vertex* temp1 = this->findVertex(w1);
     vertex* temp2 = this->findVertex(w2);
-    std::cout << "Size: " << temp1->edges.size() << std::endl;
     int count = 0;
     for (edge x : temp1->edges) {
-      // std::cout << x.v->word << std::endl;
       if (x.v == temp2) {
         return &temp1->edges.at(count);
       }
@@ -72,22 +69,18 @@ void Generator::test() {
   this->addVertex("hello");
   this->addVertex("world");
   this->addVertex("hello");
-  std::cout << this->isVertex("hello") << std::endl;
   vertex* v = this->findVertex("hello");
   vertex* test = this->findVertex("world");
   std::cout << "Vertex: " << v->word << std::endl;
   edge e = edge(test, 1);
   v->edges.push_back(e);
-  std::cout << "Size: " << v->edges.size() << std::endl;
   vertex* t = this->findVertex("hello");
-  std::cout << "Size: " << t->edges.size() << std::endl;
   edge x = v->edges.back();
-  std::cout << this->isEdge("hello", "world") << std::endl;
   std::cout << "Edge Vertex: " << x.v->word << std::endl;
   edge* a = this->findEdge("hello", "world");
   std::cout << "Edge Vertex: " << a->v->word << std::endl;
   this->addEdge("world", "hello");
   this->addEdge("hello", "world");
   edge* t_edge = this->findEdge("hello", "world");
-  std::cout << t_edge->count << std::endl;
+  std::cout << "Edge Count: " << t_edge->count << std::endl;
 }
