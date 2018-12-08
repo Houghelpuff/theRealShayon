b5568061e5054f5b4e8942a278619cfd758aec4b
nickhans <nickhanselman@icloud.com>
2018-12-08 03:07:15 -0700

    added check to make sure generatedSentence is in tweet char lim

--- Main.cpp
+++ Main.cpp
@@ -5,7 +5,8 @@ int main() {
   g.populate("test.txt");
   g.setProbability();
   std::cout << std::endl;
-  g.generateSentence();
+  while (g.generateSentence().length() > 280)
+    ;
   // g.test();
   return 0;
 }
